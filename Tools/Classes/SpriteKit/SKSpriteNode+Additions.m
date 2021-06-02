74d5569183bce64f483acc05fef6866329d417fb
xiang.song <xiang.song@dianping.com>
2021-06-02 14:44:21 +0800

    dev 6.2:修复坐标系计算bug加上锚点

--- Tools/Classes/SpriteKit/SKSpriteNode+Additions.m
+++ Tools/Classes/SpriteKit/SKSpriteNode+Additions.m
@@ -10,51 +10,51 @@
 @implementation SKSpriteNode (Additions)
 
 - (void)setLeft:(CGFloat)left {
-    self.position = CGPointMake(left, self.position.y);
+    self.position = CGPointMake(left + self.anchorPoint.x * self.size.width, self.position.y);
 }
 
 - (CGFloat)left {
-    return self.position.x;
+    return self.position.x - self.anchorPoint.x * self.size.width;
 }
 
 - (void)setRight:(CGFloat)right {
-    self.position = CGPointMake(right - self.size.width, self.position.y);
+    self.position = CGPointMake(right - (1 - self.anchorPoint.x) * self.size.width, self.position.y);
 }
 
 - (CGFloat)right {
-    return self.position.x + self.size.width;
+    return self.position.x + (1 - self.anchorPoint.x) * self.size.width;
 }
 
 - (void)setTop:(CGFloat)top {
-    self.position = CGPointMake(self.position.x, top - self.size.height);
+    self.position = CGPointMake(self.position.x, top - (1 - self.anchorPoint.y) * self.size.height);
 }
 
 - (CGFloat)top {
-    return self.position.y + self.size.height;
+    return self.position.y + (1 - self.anchorPoint.y) * self.size.height;
 }
 
 - (void)setBottom:(CGFloat)bottom {
-    self.position = CGPointMake(self.position.x, bottom);
+    self.position = CGPointMake(self.position.x, bottom + self.anchorPoint.y * self.size.height);
 }
 
 - (CGFloat)bottom {
-    return self.position.y;
-}
-
-- (void)setCenterX:(CGFloat)centerX {
-    self.position = CGPointMake(centerX - self.size.width / 2.0, self.position.y);
+    return self.position.y - self.anchorPoint.y * self.size.height;
 }
 
 - (CGFloat)centerX {
-    return self.position.x + self.size.width / 2.0;
-}
-
-- (void)setCenterY:(CGFloat)centerY {
-    self.position = CGPointMake(self.position.x, centerY - self.size.height /2.0);
+    return self.left + self.size.width / 2.0;
 }
 
 - (CGFloat)centerY {
-    return self.position.y + self.size.height / 2.0;
+    return self.bottom + self.size.height / 2.0;
+}
+
+- (void)setCenterX:(CGFloat)centerX {
+    self.position = CGPointMake(centerX - self.size.width / 2.0 + self.anchorPoint.x * self.size.width, self.position.y);
+}
+
+- (void)setCenterY:(CGFloat)centerY {
+    self.position = CGPointMake(self.position.x, centerY - self.size.height /2.0 + self.anchorPoint.y * self.size.height);
 }
 
 - (void)setWidth:(CGFloat)width {
